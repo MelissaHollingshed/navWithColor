e7428d559ad4e80f9d4b726d72225b2d3ac5fb66
MelissaHollingshed <mahv87@mail.missouri.edu>
2018-12-11 14:29:18 -0600

    the right files

---
+++ navColors/navWithColors/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  naveWithColors
+//
+//  Created by Melissa Hollingshed on 10/17/18.
+//  Copyright © 2018 Melissa Hollingshed. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+}
